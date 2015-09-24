3ed608662f71e8641f40a5168c3a61a8241ba82c
bkillenit <blaine.j.killen@gmail.com>
2015-09-24 15:13:49 -0700

    load balancer can add and remove loads from a process and then find the best process to put work on next

--- loadBalancer.js
+++ loadBalancer.js
@@ -19,16 +19,22 @@ LoadBalancer.prototype.leftChild(i) = function() {
   return 2*i + 1;
 }
 
-LoadBalancer.prototype.leftChild(i) = function() {
+LoadBalancer.prototype.rightChild(i) = function() {
   return 2*i + 2;
 }
 
 LoadBalancer.prototype.swapDirection(i, count) = function() {
-  if( count > this.priorityQueue[this.leftChild(i)][0] ) {
-    return this.leftChild(i);
-  } else if( count > this.priorityQueue[this.rightChild(i)][0] ) {
-    return this.rightChild(i);
+  if( count > this.priorityQueue[this.leftChild(i)][1] ) {
+    return 'left';
+  } else if( count > this.priorityQueue[this.rightChild(i)][1] ) {
+    return 'right';
   }
+}
+
+LoadBalancer.prototype.swapIndicesOnArray= function(idx1, idx2, array) {
+  var temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
 }
 
 // when a user adds load to the process doing the least amount of work,
@@ -39,13 +45,54 @@ LoadBalancer.prototype.addLoadToBestProcess = function(processName) {
   processTuple[0]++;
 
   var index = 0;
-  var direction = swapDirection(index, processTuple[0]);
+  var direction = swapDirection(index, processTuple[1]);
 
   while(direction) {
-    
+    var swapIdx;
+
+    if(direction === 'left'){
+      swapIdx = leftChild(index);
+    }
+    else {
+      swapIdx = rightChild(index);
+    }
+
+    swapIndicesOnArray(index, swapIdx, this.priorityQueue);
+    index = swapIdx;
+
+    direction = swapDirection(index, processTuple[1]);
   }
+
+  return processTuple[0];
 }
 
-LoadBalancer.prototype.removeLoadFromProcess = function() {
+LoadBalancer.prototype.removeLoadFromProcess = function(processName) {
+  var recurseDFS = function(index) {
+    var curProcess = this.priorityQueue[index];
+    if(curProcess[0] === processName) {
+      curProcess[1]++;
+      return true;
+    }
 
+    var leftSwap = this.leftChild(index)]);
+    var rightSwap = this.rightChild(index);
+    var child;
+
+    if(recurseDFS(leftSwap)) {
+      if(this.priorityQueue[leftSwap][1] < curProcess[1]) {
+        swapIndicesOnArray(index, leftSwap, this.indicesArray);
+        return true;
+      }
+    }
+    else if(recurseDFS(rightSwap)) {
+      if(this.priorityQueue[rightSwap][1] < curProcess[1]) {
+        swapIndicesOnArray(index, rightSwap, this.indicesArray);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  recurseDFS(0);
 }
