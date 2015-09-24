97b288a82ee1a0995e376d488f4bb273af9c480c
bkillenit <blaine.j.killen@gmail.com>
2015-09-24 12:00:50 -0700

    further progress made on the load balancer

--- loadBalancer.js
+++ loadBalancer.js
@@ -15,15 +15,35 @@ LoadBalancer.prototype.findMin = function() {
   return this.priorityQueue[0];
 }
 
-LoadBalancer.prototype.addLoadToProcess = function(processName) {
-  var target = processName;
-  var start = this.priorityQueue[0];
+LoadBalancer.prototype.leftChild(i) = function() {
+  return 2*i + 1;
+}
 
-  while(target !== start[0]) {
+LoadBalancer.prototype.leftChild(i) = function() {
+  return 2*i + 2;
+}
 
+LoadBalancer.prototype.swapDirection(i, count) = function() {
+  if( count > this.priorityQueue[this.leftChild(i)][0] ) {
+    return this.leftChild(i);
+  } else if( count > this.priorityQueue[this.rightChild(i)][0] ) {
+    return this.rightChild(i);
   }
+}
 
-  recursiveDFS(target, start);
+// when a user adds load to the process doing the least amount of work,
+// make sure to increase the work count on the process and put process now doing
+// the least amount of work to the top of the tree.
+LoadBalancer.prototype.addLoadToBestProcess = function(processName) {
+  var processTuple = this.findMin();
+  processTuple[0]++;
+
+  var index = 0;
+  var direction = swapDirection(index, processTuple[0]);
+
+  while(direction) {
+    
+  }
 }
 
 LoadBalancer.prototype.removeLoadFromProcess = function() {
