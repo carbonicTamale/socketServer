30298f24a09c06b6036c243ee77db7a05aeba198
bkillenit <blaine.j.killen@gmail.com>
2015-09-25 15:16:08 -0700

    trying something new

--- child.js
+++ child.js
@@ -1,4 +1,4 @@
-process.on('message', function(m, data, room, socket) {
+process.on('message', function(m, data, room) {
   if(m === 'socket') {
     console.log(m, data, room, socket)
     socket.broadcast.emit(room + ' event', data);
