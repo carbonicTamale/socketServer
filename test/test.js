26fe457e2ab63bfd91a681779b4e2da957012690
bkillenit <blaine.j.killen@gmail.com>
2015-09-29 09:46:33 -0700

    included shell for testing

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+app = require('../server.js');
+
+describe('Load Balancer', function () {
+
+  it('', function (done) {
+
+  });
+
+  it('' , function (done) {
+
+  });
+
+  it('' , function (done) {
+
+  });
+
+  it('', function (done) {
+
+  });
+
+  it('', function (done) {
+
+  });
+
+});
+
